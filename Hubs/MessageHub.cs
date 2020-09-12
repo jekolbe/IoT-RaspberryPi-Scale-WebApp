b9e80854ee24991529f36c8fa19369d5017e2ec4
Jenö Kolbe <jenoe.kolbe@gmail.com>
2020-09-12 17:13:06 +0200

    Initial Commit

---
+++ Hubs/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace IotSignalRApp.Hubs
+{
+    public class MessageHub : Hub
+    {
+        //The SendMessage method can be called by a connected client to send a message to all clients
+        public async Task SendMessage(string device, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", device, message);
+        }
+    }
+}
---
+++ obj/Release/netcoreapp2.1/PubTmp/Out/wwwroot/js/messages.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messageHub").build();
+
+connection.start().then(function () {
+    console.log("connected");
+});
+
+connection.on("ReceiveMessage", function (device, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = device + " says " + msg;
+    var json = JSON.parse(msg);
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("weight").innerHTML = Math.round(json.weight) + " Gramm";
+    //document.getElementById("messagesList").appendChild(li);
+});
